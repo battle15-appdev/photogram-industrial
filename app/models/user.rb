c59386ac100aaafa44e2cbd28a590747849abb83
battle15 <mbattle15@student.ccc.edu>
2022-11-29 04:26:30 +0000

    Generated follow requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :own_photos, class_name: "Photo", foreign_key: "ownder_id"
-  
-  has_many : comments, foreign_key: "author_id"
+  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
+
+  has_many :comments, foreign_key: "author_id"
+
+
 
 end
