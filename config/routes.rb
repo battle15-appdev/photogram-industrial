7ce472a61774b5b991920b7c843eaa2b7851195d
battle15 <mbattle15@student.ccc.edu>
2022-12-05 13:58:26 +0000

    Added liked and feed

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,13 @@ Rails.application.routes.draw do
   #use "resources :users" if all 7 routes are needed, if not then specify which routes are needed with "only: :action_name"
   #resources :users, only: :show
 
-  get "/:username" => "users#show"
+  get ":username/liked" => "photos#liked", as: :liked_photos
+  get ":username/feed" => "photos#feed", as: :liked_photos
+  
+  #list groups
+  get ":username/followers" => "photos#followers", as: :liked_photos
+  get ":username/following" => "photos#following", as: :liked_photos
+
+  get "/:username" => "users#show", as: :user
 
 end
